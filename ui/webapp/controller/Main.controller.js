83e46bda46506ff71a142d6e613f599a6b51c8c2
Martin Pankraz <mapankra@microsoft.com>
2019-12-02 11:53:36 +0000

    intial

---
+++ ui/webapp/controller/Main.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("sapcp.azure.cf.ui.controller.Main", {
+		onInit: function () {
+
+		},
+		
+		openGitHub: function(oEvent){
+			window.open("https://github.com/MartinPankraz/UI5OnAzure");
+		}
+	});
+});
---
+++ ui/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"sapcp/azure/cf/ui/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
