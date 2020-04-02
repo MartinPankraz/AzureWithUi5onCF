593ac91577755a6825a4fcd45db21a3f58a7ff47
Martin Pankraz <mapankra@microsoft.com>
2020-04-02 16:55:34 +0200

    app insights npm module added

--- ui/webapp/controller/Main.controller.js
+++ ui/webapp/controller/Main.controller.js
@@ -1,3 +1,20 @@
+const config = require('./config.js');
+let appInsights = require('applicationinsights');
+appInsights.setup(config.appInsights)
+.setAutoDependencyCorrelation(true)
+    .setAutoCollectRequests(true)
+    .setAutoCollectPerformance(true)
+    .setAutoCollectExceptions(true)
+    .setAutoCollectDependencies(true)
+    .setAutoCollectConsole(true)
+    .setUseDiskRetryCaching(false)
+    .start();
+//Names for Azure AppInsights application map
+appInsights.defaultClient.addTelemetryProcessor(envelope => {
+    envelope.tags["ai.cloud.role"] = "myappinaws";
+    envelope.tags["ai.cloud.roleInstance"] = "myawsapp-1"
+});
+
 sap.ui.define([
 	"sap/ui/core/mvc/Controller"
 ], function (Controller) {
